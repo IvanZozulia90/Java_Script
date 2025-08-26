c7be9e3a870d4ff02e66df8591405e44c74d45fe
Ivan <ivan.zozulia90@gmail.com>
2025-08-26 20:58:33 +0300

    cycle

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
-const students = [];
+// const students = [];
 
-students.push({name: "Ivan", age: 35});
-students.push({name: "Den", age: 25});
+// students.push({name: "Ivan", age: 35});
+// students.push({name: "Den", age: 25});
+// students.push({name: "asdf", age: 31})
 
+// console.log(students)
+
+let people = {
+    Ivan: 35,
+    Olga: 32,
+    Vikki: 5
+}
+
+for(let name in people) {
+    if (people[name] == 6) {
+        console.log(name)
+    }else{
+        console.log("Користувача не знайдено")
+    }
+}
 
