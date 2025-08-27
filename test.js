41c349885fe620629837fb3cec7673bf4c15784f
Ivan <ivan.zozulia90@gmail.com>
2025-08-27 11:15:58 +0300

    add input and function

--- test.js
+++ test.js
@@ -11,12 +11,37 @@ let people = {
     Olga: 32,
     Vikki: 5
 }
+// let found = false
 
-for(let name in people) {
-    if (people[name] == 6) {
-        console.log(name)
-    }else{
-        console.log("Користувача не знайдено")
+// for(let name in people) {
+//     if (people[name] === 6) {
+//         console.log(name);
+//         found = true;
+//     }else{
+//         if(!found){
+//             console.log("Користувача не знайдено")
+//         }
+//     }
+// }
+
+function findNameByAge(){
+    const input = document.getElementById("ageInput");
+    const result = document.getElementById("result");
+    const ageToFind = Number(input.value);
+
+    let found = false
+
+    for (let name in people) {
+        if (people[name] == ageToFind) {
+            result.textContent = `Користувач ${name}`;
+            found = true;
+            break
+        }else{
+            if(!found){
+                result.textContent = "Користувача не знайдено";
+            }
+        }input.value = "";
     }
 }
+
 
